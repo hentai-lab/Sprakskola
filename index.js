a1e1cf2830e5fddc9a7b06509d4379d490c3bcf6
Matheus Marques Polillo <matheus_polillo@yahoo.com>
2019-10-01 15:54:34 -0300

    Images, html pages and simple css added/Index html and js changed

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var Express = require('express');
 var app = Express();
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 
 app.use(Express.static(__dirname + '/public'));
-app.get('/game', function (req, res){
-    res.send('Game');
+app.set('views', __dirname + '/public');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.get('/index', function(req, res) {
+    res.render('index.html');
 });
-app.get('/chat', function (req, res){
-    res.send('Chat');
+app.get('/game', function(req, res) {
+    var imgId = Math.floor(Math.random() * 7);
+    res.render('game.html', {'image': '/styles/images/' + imgId + '.png'});
+});
+app.get('/chat', function(req, res) {
+    res.render('chat.html');
 });
 
 app.listen(port, function() {
-    console.log("App iniciada: http://localhost:" + port);
+    console.log('App iniciada: http://localhost:' + port);
 });
