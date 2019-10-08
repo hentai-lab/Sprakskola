b546c0bf68f533ccaa8433f9f54ca4aebae96584
Polillo, M. M <matheus_polillo@yahoo.com>
2019-10-07 23:01:10 -0300

    Watson chat added (incomplete)

--- index.js
+++ index.js
@@ -1,31 +1,61 @@
-var Express = require('express');
-var app = Express();
-var port = process.env.PORT || 3000;
+const AssistantV2 = require('ibm-watson/assistant/v2');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-app.use(Express.static(__dirname + '/public'));
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/public');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.get('/index', function(req, res) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const watson_api_key = '';
+const watson_assistant_id = '';
+const watson_workspace_id = '';
+
+const port = process.env.PORT || 3000;
+
+const assistant = new AssistantV2({
+    iam_apikey: watson_api_key,
+    url: 'https://gateway.watsonplatform.net/assistant/api/',
+    version: '2018-09-19'
+});
+
+app.get('/index', (req, res) => {
     res.render('index.html');
 });
 
-app.get('/game', function(req, res) {
+app.get('/game', (req, res) => {
     var imgId = Math.floor(Math.random() * 7);
     res.render('game.html', {'image': '/styles/images/' + imgId + '.png'});
 });
 
-app.get('/chat', function(req, res) {
+app.get('/chat', (req, res) => {
     res.render('chat.html');
+    /* var result = req.params.input_field;
+    console.log(result);
+    res.end(); */
 });
 
-app.post('/message', function(req, res) {
-    var AssistantV1 = require('ibm-watson/assistant/v1');
+app.post('/message', (req, res) => {
+    var result = req.body.input_field;
+    console.log(result);
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log('App iniciada: http://localhost:' + port);
+});
+
+/*
+var AssistantV1 = require('ibm-watson/assistant/v1');
     
-    const watson_api_key = 'RvHtP5bqBKk_EJekpmhxwC3ErPh3MFpssNEqrxv08zJw';
-    const watson_assistant_id = '748bda65-ebe4-472e-95b0-786cfc990342';
-    const watson_workspace_id = 'e91562c9-4b92-446a-8b0e-d09f2eceb50c';
+    const watson_api_key = '';
+    const watson_assistant_id = '';
+    const watson_workspace_id = '';
 
     var inputMessage = document.getElementById('message').value;
 
@@ -44,8 +74,4 @@ app.post('/message', function(req, res) {
     }).catch(err => {
         console.log(err);
     });
-});
-
-app.listen(port, function() {
-    console.log('App iniciada: http://localhost:' + port);
-});
+*/
